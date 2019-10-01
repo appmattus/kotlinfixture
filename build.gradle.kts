a4bafeca767aef4d1d9bd931678fe157da443664
Matthew Dolan <info@mattdolan.com>
2019-10-01 06:06:48 +0100

    Restructure with a module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,47 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50" apply false
+    id("com.github.ben-manes.versions") version "0.25.0"
+    id("io.gitlab.arturbosch.detekt") version "1.0.1"
+    id("com.github.kt3k.coveralls") version "2.8.4" apply false
 }
 
 //group 'com.appmattus.fixturekotlin'
 //version '1.0.0'
 
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        jcenter()
+        mavenCentral()
+    }
 }
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("io.github.classgraph:classgraph:4.8.47")
-    implementation(kotlin("reflect"))
-
-    testImplementation("junit:junit:4.12")
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
-    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+task("clean", type = Delete::class) {
+    delete(rootProject.buildDir)
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+tasks.withType(DependencyUpdatesTask::class.java).all {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
+                    .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
+                    .any { it.matches(candidate.version) }
+
+                if (rejected) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+}
+
+detekt {
+    input = files("$projectDir")
+
+    buildUponDefaultConfig = true
+
+    // To override MaxLineLength:excludeCommentStatements
+    //config = files("detekt-config.yml")
 }
