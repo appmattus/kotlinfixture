7cffb888b2fd953c448b820e3e14785c66bda010
Matthew Dolan <info@mattdolan.com>
2019-10-01 06:08:13 +0100

    Restructure with a module (#1)

---
+++ fixture/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
+//group 'com.appmattus.fixturekotlin'
+//version '1.0.0'
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.github.classgraph:classgraph:4.8.47")
+    implementation(kotlin("reflect"))
+
+    testImplementation("junit:junit:4.12")
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
