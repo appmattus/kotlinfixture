92c20a4d2752eb3b833d18c0dde44a0c975857f8
Matthew Dolan <matt.dolan@babylonhealth.com>
2020-05-02 09:55:07 +0100

    Upgrade classgraph

--- fixture/build.gradle.kts
+++ fixture/build.gradle.kts
@@ -26,7 +26,7 @@ apply(from = "$rootDir/gradle/scripts/jacoco.gradle.kts")
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.github.classgraph:classgraph:4.8.77")
+    implementation("io.github.classgraph:classgraph:4.8.78")
     implementation(kotlin("reflect"))
 
     compileOnly("joda-time:joda-time:2.10.6")
