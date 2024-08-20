77bf96cf2e4e72b46797aab843df3912016226b9
Marco De Toma <marco.de.toma@fastned.nl>
2024-08-20 07:57:28 +0200

    build: add compatibility to java 17

--- fixture/build.gradle.kts
+++ fixture/build.gradle.kts
@@ -54,12 +54,12 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
 }
 
 tasks.named("check") {
